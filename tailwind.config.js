7b17e1fc80c635beb16e62c70e9561324fe756db
purvikajagtap <fs21co004.purvikajagtap@gmail.com>
2024-01-29 00:36:16 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      serif: ["Playfair Display", ...defaultTheme.fontFamily.sans],
+      sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
+  },
+  plugins: [],
+}
